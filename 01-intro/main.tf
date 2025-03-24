e4982c4b92ff3845b27d3cb900a05094ed968bca
salllmanshaik <hello.salmanshaik@gmail.com>
2025-03-24 13:50:22 +0530

    user

--- 01-intro/main.tf
+++ 01-intro/main.tf
@@ -1,4 +1,6 @@
 #Now suppose I want to create a local file, I will make use of local_file resource. Lets see
+provider local {}
+
 
 resource "local_file" "first" {
   content  = "Welcome to learning terraform by sallman"
