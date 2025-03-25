fcc134286960ead4ffb6cb4de780dcb23eb39e9f
salllmanshaik <hello.salmanshaik@gmail.com>
2025-03-25 14:33:21 +0530

    user

--- 02-loops-count/main.tf
+++ 02-loops-count/main.tf
@@ -1,3 +1,18 @@
-resource "null_resource" "dummy" {
+resource "null_resource" "dummy" {   #we are using null resource
+  count = length(var.x)
 
+  provisioner "local-exec" {
+    command = "echo ${var.x[count.index]}"   #This will print for 4 times because we have 4 values in x
+  }
+
+}
+
+
+variable "x" {
+  default = [
+    1,
+    200,
+    300,
+    400
+  ]
 }
